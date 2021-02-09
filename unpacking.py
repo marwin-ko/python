1523bf9215db5b874a9e973aa982dcfeff31fb2c
Marwin Ko <marwin.r.ko@gmail.com>
2021-02-08 21:27:39 -0800

    Update unpacking.py

--- unpacking.py
+++ unpacking.py
@@ -1,3 +1,16 @@
 # unpack range object
-[*range(1,10)]
+[*range(1,3)]
+# [1,2]
 
+# unpack enumerate
+[*enumerate('abc')]
+[(0, 'a'), (1, 'b'), (2, 'c')]
+
+# unpack enumerate and turn into dict
+dict([*enumerate('abc')])
+{0: 'a', 1: 'b', 2: 'c'}
+
+
+# TODO
+# - funct w/ *args
+# - funct w/ **kwargs
