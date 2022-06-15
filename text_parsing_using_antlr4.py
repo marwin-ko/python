0c5d47aff4bcb6d2d90581962d1ce49adf2332aa
Marwin Ko <marwin.r.ko@gmail.com>
2022-06-15 10:48:15 -0700

    Create text_parsing_using_antlr4.py

---
+++ text_parsing_using_antlr4.py
@@ -0,0 +1,7 @@
+# https://github.com/antlr/antlr4/blob/master/doc/python-target.md
+
+"""
+This is a package that can help create customize parsing language.
+This may be useful if there is a lot of mapping from text to another lanague or syntax.
+Just know that this exists....implement when sensible.
+"""
